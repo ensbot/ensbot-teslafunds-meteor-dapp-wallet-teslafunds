f194d81f48295b6ac4c252929f76fce2f9f629c5
Jan <janonorati@yahoo.it>
2019-01-28 02:11:22 +0100

    Update 1_web3Init.js

--- app/client/lib/ethereum/1_web3Init.js
+++ app/client/lib/ethereum/1_web3Init.js
@@ -2,4 +2,4 @@
 if(typeof web3 !== 'undefined')
   web3 = new Web3(web3.currentProvider);
 else
-  web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.1.54:4949"));
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:4949"));
